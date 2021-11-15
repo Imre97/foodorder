193d3238cf5eab1329b6865e80cb7e08b33fc5b0
Imi <imre.varga97@gmail.com>
2021-11-15 21:48:40 +0100

    handel error

--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -8,17 +8,24 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([])
   const [loading, setLoading] = useState(false)
+  const [error, setError] = useState()
 
 
   useEffect(() => {
     setLoading(true)
+    setError("")
     const getMeals = async () => {
       const data = await fetch('https://mealsorder-8a023-default-rtdb.firebaseio.com/meals.json')
+
+      if (!data.ok) {
+        throw new Error('Something went wrong')
+      }
+
       const res = await data.json()
 
       const loadedData = []
 
-      for(const key in res) {
+      for (const key in res) {
         loadedData.push({
           id: key,
           name: res[key].name,
@@ -32,21 +39,27 @@ const AvailableMeals = () => {
 
     }
 
-    getMeals()
+      getMeals().catch((error) => {
+        setLoading(false)
+        setError(error.message)
+      })
+
   }, [])
+
 
   return (
     <section className={classes.meals}>
       <ul>
         <Card>
+          {error && <h2>{error}</h2>}
           {loading && <h2>Loading...</h2>}
           {meals.map(meal => {
-            return <MealItem 
-              name={meal.name} 
-              id={meal.id} 
-              key={meal.id} 
-              description={meal.description} 
-              price={meal.price} 
+            return <MealItem
+              name={meal.name}
+              id={meal.id}
+              key={meal.id}
+              description={meal.description}
+              price={meal.price}
             />
           })}
         </Card>
